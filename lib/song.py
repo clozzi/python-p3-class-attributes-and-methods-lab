87111fdb62cc23095cec3606e7c6a7cdc39c1ecc
Chris Lozzi <clozzi12@gmail.com>
2023-12-07 18:10:19 -0500

    missing genre/artist counts

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,24 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    genre_count = {}
+    artists = []
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+        self.genres.append(genre)
+        self.artists.append(artist)
+        self.genre = genre
+        self.name = name
+        self.artist = artist
+
+    @classmethod
+    def add_song_to_count(cls, inc=1):
+        cls.count += inc
+
+    def add_to_genre_count():
+        pass
+
+    def add_to_artist_count():
+        pass
